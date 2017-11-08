bc543eec887b9b290e15c3fb615c0cdab85ea613
Aly Baughman <baugh107@mail.chapman.edu>
2017-11-08 05:24:16 +0000

    first draft

--- src/stack/stack.c
+++ src/stack/stack.c
@@ -2,23 +2,26 @@
 #include <stdlib.h>
 #include "stack.h"
 
+// create a new stack 
 STACK new_stack() {
-    STACK new = NULL;
-    return new;
+    STACK new = NULL; // stack is empty
+    return new; // return the empty stack 
 }
 
-void push_stack(STACK *s, SVALUE val) {
-    STACK new = (STACK) malloc(sizeof(struct stack));
+// add a value to the stack 
+void push_stack(STACK *s, SVALUE val) { 
+    STACK new = (STACK) malloc(sizeof(struct stack)); 
     if (new == NULL) {
         fprintf(stderr, "ERROR: Unable to push onto stack\n");
         return;
     }
-    new->data = val;
-    new->head = *s;
-    *s = new;
+    new->data = val; // set the value in the new stack
+    new->head = *s; // set the head of the stack
+    *s = new; // point to the new stack
 }
 
-SVALUE pop_stack(STACK *s) {
+// remove a value from the stack by "popping" it off
+SVALUE pop_stack(STACK *s) { 
     SVALUE val;
     STACK temp;
     if (*s == NULL) {
@@ -33,3 +36,21 @@ SVALUE pop_stack(STACK *s) {
     }
     return val;
 }
+
+// print svalue
+void print_svalue(SVALUE val) {
+    printf("%d", val); // print val 
+}
+
+// print stack
+void print_stack(STACK s) {
+    while (s->head != NULL) { // while there is a value and not empty/null
+        print_svalue(s->data); // print the data of the stack from the top 
+        printf("\t"); // tab for formatting 
+        s = s->head; // s is now to the head, so printing from the top to bottom in this loop
+
+    }
+    print_svalue(s->data); // exit loop and print
+    printf("\n"); // print a new line 
+}
+
